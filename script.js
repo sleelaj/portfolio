0094b4e1eba6f1d7042a1b7f7dbf2f375d494d3f
Saileela Josyula <saileelajosyula@Saileelas-Air.home>
2024-12-30 16:20:36 -0500

    Styling7  navigation bar to the existing portfolio

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     AOS.init(); // Initialize AOS (Animate on Scroll)
 });
 
-// Typed.js for Animated Text
 var typed = new Typed('#animated-text', {
     strings: ["a Full Stack Developer", "a Software Engineer", "an Innovator"],
     typeSpeed: 50,
